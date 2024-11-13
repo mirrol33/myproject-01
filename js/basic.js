3104b2fd0620ed83d0e788a70b0ec2d5fd6e7e99
freelife11 <mirrol33@gmail.com>
2024-11-13 17:52:47 +0900

    11.13 업데이트2

--- js/basic.js
+++ js/basic.js
@@ -14,3 +14,15 @@ window.addEventListener('load', function() {
         }
     });
 });
+
+// lenis.js
+const lenis = new Lenis({
+    duration: 2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  });
+  
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  requestAnimationFrame(raf);
