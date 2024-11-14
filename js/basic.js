3ce04d2146f4a44bb7e677dc97e96dbf61c2654f
freelife11 <mirrol33@gmail.com>
2024-11-14 17:59:42 +0900

    11.14 업데이트7

--- js/basic.js
+++ js/basic.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function () {
+  // 외부 html 파일 불러오기!
   var allElements = document.getElementsByTagName("*");
   Array.prototype.forEach.call(allElements, function (el) {
     var includePath = el.dataset.includePath;
@@ -12,17 +13,30 @@ window.addEventListener("load", function () {
       xhttp.open("GET", includePath, true);
       xhttp.send();
     }
+  }); ////
+
+  // lenis.js 스크롤을 부드럽게하는 효과
+  const lenis = new Lenis({
+    duration: 2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
   });
-});
 
-// lenis.js
-const lenis = new Lenis({
-  duration: 2,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-});
-
-function raf(time) {
-  lenis.raf(time);
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
   requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
+
+  // lenis 멈춤
+  lenis.stop();
+  // 스크롤바 없애기
+  document.body.style.overflow = "hidden";
+
+  this.setTimeout(() => {
+    // lenis 시작
+    lenis.start();
+    // 스크롤바 생성
+    document.body.style.overflow = "auto";
+  }, 5000); // 5초후 실행
+
+}); //// load event listener ////
